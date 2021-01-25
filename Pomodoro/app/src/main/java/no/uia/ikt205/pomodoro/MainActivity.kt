b3215c9ae872ff5d1f65a80eb354906077620db1
Vegard Dale <vegard17@student.uia.no>
2021-01-25 17:18:51 +0100

    added timer options for 30,60,90,120 min

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -13,10 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var coutdownDisplay:TextView
+    lateinit var countdownDisplay:TextView
+    lateinit var timerButton30:Button
+    lateinit var timerButton60:Button
+    lateinit var timerButton90:Button
+    lateinit var timerButton120:Button
 
-    val timeToCountDownInMs = 5000L
-    val timeTicks = 1000L
+    var timeToCountDownInMs:Long = 0L
+    var isActive:Boolean = false
+
+    val timeTicks:Long = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +30,50 @@ class MainActivity : AppCompatActivity() {
 
        startButton = findViewById<Button>(R.id.startCountdownButton)
        startButton.setOnClickListener(){
-           startCountDown(it)
+           if(!isActive) startCountDown(it, timeToCountDownInMs)
        }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
+        timerButton30 = findViewById<Button>(R.id.timerButton30)
+        timerButton30.setOnClickListener(){
+            if(!isActive){
+                timeToCountDownInMs = 1800000L
+                updateSelectedTime(timeToCountDownInMs)
+            }
+        }
+
+        timerButton60 = findViewById<Button>(R.id.timerButton60)
+        timerButton60.setOnClickListener(){
+            if(!isActive){
+                timeToCountDownInMs = 3600000L
+                updateSelectedTime(timeToCountDownInMs)
+            }
+        }
+
+        timerButton90 = findViewById<Button>(R.id.timerButton90)
+        timerButton90.setOnClickListener(){
+            if(!isActive){
+                timeToCountDownInMs = 5400000L
+                updateSelectedTime(timeToCountDownInMs)
+            }
+        }
+
+        timerButton120 = findViewById<Button>(R.id.timerButton120)
+        timerButton120.setOnClickListener(){
+            if(!isActive){
+                timeToCountDownInMs = 7200000L
+                updateSelectedTime(timeToCountDownInMs)
+            }
+        }
+
+       countdownDisplay = findViewById<TextView>(R.id.countDownView)
     }
 
-    fun startCountDown(v: View){
-
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+    fun startCountDown(v: View, timeInMs: Long){
+        isActive = true
+        timer = object : CountDownTimer(timeInMs, timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                isActive = false
             }
 
             override fun onTick(millisUntilFinished: Long) {
@@ -43,10 +82,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         timer.start()
+
     }
 
     fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 
+    fun updateSelectedTime(timeInMs: Long){
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+    }
 }
